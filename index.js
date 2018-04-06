a8d29c0fb395f087031634054847c9a84efe4482
ChekaLI <34219823+ChekaLI@users.noreply.github.com>
2018-04-06 19:10:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const robot = new Discord.Client();
 
-robot.login("NDMxNTk4NDkzNjI3NDQ5MzQ0.DahFEw.NTyvP22YO5bY83-QeNEy6mZ7qoQ");
+robot.login("NDMxNTk4NDkzNjI3NDQ5MzQ0.DakswQ.iT-nhkYQOATz1ASPNEeRuiVIUnM");
 
 robot.on('message', (message) => {
     if(message.content == "test"){
